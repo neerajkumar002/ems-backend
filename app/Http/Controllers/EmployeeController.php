a6a153e297a0cc28b68cd8d9d29202840f3bed65
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-05-05 21:40:37 +0530

    Add employeeController

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+
+        $employee = Employee::all();
+
+        return response()->json(["data" => $employee], 200);
+    }
+}
