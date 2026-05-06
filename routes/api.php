6981b24de9fa424f041e915c3859085548176a54
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-05-06 17:29:11 +0530

    add api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
- <?php
+<?php
 
-    use App\Http\Controllers\EmployeeController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
 
-    Route::apiResource("employees", EmployeeController::class);
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
 
-     
-    
+//protected route
+Route::middleware("auth:sanctum")->post("/logout", [AuthController::class, "logout"]);
 
-    ?>
+Route::resource("employees", EmployeeController::class);
